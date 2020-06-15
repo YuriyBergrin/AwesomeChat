ddcf29c78d6980b66753fc6eb335cccc9e9f652c
YuriyBergrin <bergrin1991@gmail.com>
2020-06-15 22:11:15 +0300

    update

--- app/src/main/java/com/bergrin/awesomechat/UserListActivity.java
+++ app/src/main/java/com/bergrin/awesomechat/UserListActivity.java
@@ -1,14 +1,86 @@
 package com.bergrin.awesomechat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 public class UserListActivity extends AppCompatActivity {
+
+    private DatabaseReference usersDatabaseReference;
+    private ChildEventListener usersChildEventListener;
+
+    private ArrayList<User> userArrayList;
+    private RecyclerView usersRecyclerView;
+    private UserAdapter userAdapter;
+    private RecyclerView.LayoutManager userLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
+
+        userArrayList = new ArrayList<>();
+
+        attachUserDataBaseReferenceListener();
+        buildRecyclerView();
+
+    }
+
+    private void attachUserDataBaseReferenceListener() {
+        usersDatabaseReference = FirebaseDatabase.getInstance().getReference().child("users");
+        if (usersChildEventListener == null) {
+            usersChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    User user = dataSnapshot.getValue(User.class);
+                    user.setAvatarMockUpResource(R.drawable.ic_baseline_person_50);
+                    userArrayList.add(user);
+                    userAdapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            };
+
+            usersDatabaseReference.addChildEventListener(usersChildEventListener);
+        }
+    }
+
+    private void buildRecyclerView() {
+        usersRecyclerView = findViewById(R.id.userListRecyclerView);
+        usersRecyclerView.setHasFixedSize(true);
+        userLayoutManager = new LinearLayoutManager(this);
+        userAdapter = new UserAdapter(userArrayList);
+        usersRecyclerView.setLayoutManager(userLayoutManager);
+        usersRecyclerView.setAdapter(userAdapter);
     }
 }
