ec659188a3c553bb3f0a53a83df2352db65cd0b0
YuriyBergrin <bergrin1991@gmail.com>
2020-08-16 14:37:40 +0300

    add message bubbles

--- app/src/main/java/com/bergrin/awesomechat/ChatActivity.java
+++ app/src/main/java/com/bergrin/awesomechat/ChatActivity.java
@@ -180,10 +180,13 @@ public class ChatActivity extends AppCompatActivity {
                 AwesomeMessage message = dataSnapshot.getValue(AwesomeMessage.class);
                 if (message.getSender().equals(auth.getCurrentUser().getUid())
                         && message.getRecipient().equals(recipientUserId)
-                ||
-                        message.getRecipient().equals(auth.getCurrentUser().getUid())
-                                && message.getSender().equals(recipientUserId)
                 ) {
+                    message.setMine(true);
+                    adapter.add(message);
+                } else if (message.getRecipient().equals(auth.getCurrentUser().getUid())
+                        && message.getSender().equals(recipientUserId)
+                ) {
+                    message.setMine(false);
                     adapter.add(message);
                 }
             }
