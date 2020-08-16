f1232cf29e087dcf371f8ddbc4643fdb65edff21
YuriyBergrin <bergrin1991@gmail.com>
2020-08-16 20:06:29 +0300

    add name bar

--- app/src/main/java/com/bergrin/awesomechat/AwesomeMessageAdapter.java
+++ app/src/main/java/com/bergrin/awesomechat/AwesomeMessageAdapter.java
@@ -2,6 +2,7 @@ package com.bergrin.awesomechat;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -16,36 +17,82 @@ import com.bumptech.glide.Glide;
 import java.util.List;
 
 public class AwesomeMessageAdapter extends ArrayAdapter<AwesomeMessage> {
-    public AwesomeMessageAdapter(Context context, int resource, List<AwesomeMessage> messages) {
-        super(context, resource, messages);
+
+    private List<AwesomeMessage> messages;
+    private Activity activity;
+
+    public AwesomeMessageAdapter(Activity activity, int resource, List<AwesomeMessage> messages) {
+        super(activity, resource, messages);
+        this.messages = messages;
+        this.activity = activity;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.message_item, parent, false);
+
+        ViewHolder viewHolder;
+        LayoutInflater layoutInflater = (LayoutInflater) activity.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+        AwesomeMessage awesomeMessage = getItem(position);
+        int layoutResource = 0;
+        int viewType = getItemViewType(position);
+
+        if (viewType == 0) {
+            layoutResource = R.layout.my_message_item;
+        } else {
+            layoutResource = R.layout.your_message_item;
         }
 
-        ImageView photoImageView = convertView.findViewById(R.id.photoImageView);
-        TextView textTextView = convertView.findViewById(R.id.textTextView);
-        TextView nameTextView = convertView.findViewById(R.id.nameTextView);
+        if (convertView != null) {
+            viewHolder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = layoutInflater.inflate(layoutResource, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }
 
-        AwesomeMessage message = getItem(position);
-
-        boolean isText = message.getImageUrl() == null;
+        boolean isText = awesomeMessage.getImageUrl() == null;
 
         if (isText) {
-            textTextView.setVisibility(View.VISIBLE);
-            photoImageView.setVisibility(View.GONE);
-            textTextView.setText(message.getText());
+            viewHolder.messageTextView.setVisibility(View.VISIBLE);
+            viewHolder.photoImageView.setVisibility(View.GONE);
+            viewHolder.messageTextView.setText(awesomeMessage.getText());
         } else {
-            textTextView.setVisibility(View.GONE);
-            photoImageView.setVisibility(View.VISIBLE);
-            Glide.with(photoImageView.getContext()).load(message.getImageUrl()).into(photoImageView);
+            viewHolder.messageTextView.setVisibility(View.GONE);
+            viewHolder.photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(viewHolder.photoImageView.getContext()).
+                    load(awesomeMessage.getImageUrl())
+                    .into(viewHolder.photoImageView);
         }
 
-        nameTextView.setText(message.getName());
-
         return convertView;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        int flag;
+        AwesomeMessage awesomeMessage = messages.get(position);
+        if (awesomeMessage.isMine()) {
+            flag = 0;
+        } else {
+            flag = 1;
+        }
+
+        return flag;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    private class ViewHolder {
+        private TextView messageTextView;
+        private ImageView photoImageView;
+
+        public ViewHolder(View view) {
+            photoImageView = view.findViewById(R.id.photoImageView);
+            messageTextView = view.findViewById(R.id.messageTextView);
+        }
     }
 }
